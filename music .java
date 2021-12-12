ccf3ff5dfc769a98b462dda62da1092b5366d540
Mehul gour <gourmehul82@gmail.com>
2021-12-12 14:59:55 +0530

    Create music .java

---
+++ music .java
@@ -0,0 +1,28 @@
+public class  music {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+    public Song(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
